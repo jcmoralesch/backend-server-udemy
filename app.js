58e8fe957a26fcf2a8f0dd6df897531dd3cd3fc5
jcmorales <jcmoralesch@hotmail.com>
2020-07-12 14:59:31 -0600

    Seccion 11 Terminada - CRUD-Imagenes

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(bodyParser.json());
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadsRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 //Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -30,6 +35,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 //Rutas
 app.use('/login', loginRoutes)
 app.use('/usuario', usuarioRoutes) //se hace uso de middeware
+app.use('/hospital', hospitalRoutes)
+app.use('/medico', medicoRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadsRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/', appRoutes) //se hace uso de middeware
 
 
